e50483598268a6e890b471289963748a645bca82
Caner Tüysüz <canertuysuz@hotmail.com>
2024-07-25 14:36:22 +0300

    remove pages and routes from App.tsx: forgotPassword, register

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Authenticated, Refine } from "@refinedev/core"
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar"
 import routerBindings, {
   CatchAllNavigate,
   NavigateToResource,
@@ -10,48 +9,40 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"
 import "./App.css"
 import { authProvider } from "./authProvider"
 import { Layout } from "./components/layout"
-import { ForgotPassword } from "./pages/forgotPassword"
 import { Login } from "./pages/login"
-import { Register } from "./pages/register"
 
 function App() {
   return (
     <BrowserRouter>
-      <RefineKbarProvider>
-        <Refine
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-          authProvider={authProvider}
-          routerProvider={routerBindings}
-        >
-          <Routes>
-            <Route
-              element={
-                <Authenticated
-                  key="authenticated-inner"
-                  fallback={<CatchAllNavigate to="/login" />}
-                >
-                  <Layout>
-                    <Outlet />
-                  </Layout>
-                </Authenticated>
-              }
-            ></Route>
-            <Route
-              element={
-                <Authenticated key="authenticated-outer" fallback={<Outlet />}>
-                  <NavigateToResource />
-                </Authenticated>
-              }
-            >
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-            </Route>
-          </Routes>
-
-          <RefineKbar />
-        </Refine>
-      </RefineKbarProvider>
+      <Refine
+        dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+        authProvider={authProvider}
+        routerProvider={routerBindings}
+      >
+        <Routes>
+          <Route
+            element={
+              <Authenticated
+                key="authenticated-inner"
+                fallback={<CatchAllNavigate to="/login" />}
+              >
+                <Layout>
+                  <Outlet />
+                </Layout>
+              </Authenticated>
+            }
+          ></Route>
+          <Route
+            element={
+              <Authenticated key="authenticated-outer" fallback={<Outlet />}>
+                <NavigateToResource />
+              </Authenticated>
+            }
+          >
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </Refine>
     </BrowserRouter>
   )
 }
